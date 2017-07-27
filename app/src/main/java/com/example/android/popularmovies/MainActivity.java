c4d3cc81f09ecccf16deafe8b15ce3a33de95755
aaroncrutchfield <aaroncrutchfield09@gmail.com>
2017-07-26 23:23:23 -0400

    Started setting up RecyclerView in onCreate method

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvMoviePosters;
+
+    // TODO: 7/26/2017 create adapter
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvMoviePosters = (RecyclerView) findViewById(R.id.rv_movie_posters);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        rvMoviePosters.setLayoutManager(gridLayoutManager);
+
+        // TODO: 7/26/2017 set adapter
     }
 }
