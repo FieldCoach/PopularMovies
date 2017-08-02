4cab1cfc83288b054ec6cc1600fcf028b081ca18
aaroncrutchfield <aaroncrutchfield09@gmail.com>
2017-08-02 00:15:42 -0400

    Error in MainActivity -trying to use onClick on a null object (etApiDialog)

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.android.popularmovies;
 
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.android.popularmovies.utilities.NetworkUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     private RecyclerView rvMoviePosters;
+    private EditText etApiDialog;
+    private Spinner spSortBy;
 
     private MoviePosterAdapter moviePosterAdapter;
+    private String sortBySelection = "Popular";
+    private String apiKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +41,110 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rvMoviePosters = (RecyclerView) findViewById(R.id.rv_movie_posters);
+//        etApiDialog = (EditText) findViewById(R.id.et_api_dialog);
+        spSortBy = (Spinner) findViewById(R.id.spinner);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         rvMoviePosters.setLayoutManager(gridLayoutManager);
 
         moviePosterAdapter = new MoviePosterAdapter();
         rvMoviePosters.setAdapter(moviePosterAdapter);
+
+        apiKey = requestApiKey();
+        if (apiKey != null) {
+            setUpSpinner();
+            getMovies();
+        }
+    }
+
+    private void setUpSpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sort_by_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spSortBy.setAdapter(adapter);
+
+        spSortBy.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                sortBySelection = adapterView.getItemAtPosition(i).toString();
+                getMovies();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //Do nothing
+            }
+        });
+    }
+
+    // TODO: 8/1/2017 prompt user for sortOrder and apiKey
+    // TODO: 8/1/2017 design UI to prompt user for sortOrder and apiKey
+    private void getMovies() {
+        URL movieRequestUrl = NetworkUtils.buildUrl(sortBySelection, apiKey);
+        new GetMoviesTask().execute(movieRequestUrl);
+    }
+
+    /**
+     * Prompts the user for their API Key. The dialog won't dismiss unless they enter one
+     * @return the API key
+     */
+    private String requestApiKey() {
+        final String[] apiKey = new String[1];
+
+        AlertDialog.Builder apiKeyDialog = new AlertDialog.Builder(this);
+        apiKeyDialog.setMessage("Enter API Key")
+                .setView(R.layout.api_dialog_layout)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Get the API Key from the user
+                        etApiDialog = (EditText) findViewById(R.id.et_api_dialog);
+                        apiKey[0] = etApiDialog.getText().toString();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Notify the user we cannot continue without the API Key
+                        Toast.makeText(getApplicationContext(), "Cannot continue without API Key", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .show();
+        Log.d(TAG, "requestApiKey() returned: " + apiKey[0]);
+        return apiKey[0];
     }
 
 
+    public class GetMoviesTask extends AsyncTask<URL, Void, String> {
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL movieRequestUrl = params[0];
+            String movieRequestResults = null;
+            try {
+                movieRequestResults = NetworkUtils.getResponseFromHttpUrl(movieRequestUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // TODO: 8/1/2017 consider returning JSON object instead of String
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(movieRequestResults);
+                Log.d(TAG, "doInBackground() returned: " + jsonObject.toString(5));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return movieRequestResults;
+        }
+
+        @Override
+        protected void onPostExecute(String strings) {
+            super.onPostExecute(strings);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,70 @@
+package com.example.android.popularmovies.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+
+/**
+ * Created by AaronC on 7/27/2017.
+ */
+
+public final class NetworkUtils {
+
+    final static String BASE_URL = "http://api.themoviedb.org/3/movie";
+    final static String QUERY_PARAMETER = "api_key";
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    /**
+     * Builds the url used to fetch popular movies from the API of themoviedb.org
+     * @param sortOrder must be either popular or top_rated
+     * @param apiKey request this from the user to prevent issues with GitHub
+     * @return the built url
+     */
+    public static URL buildUrl(String sortOrder, String apiKey) {
+        Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                .appendPath(sortOrder)
+                .appendQueryParameter(QUERY_PARAMETER, apiKey)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "buildUrl() returned: " + url);
+        return url;
+    }
+
+    /**
+     * Retrieves the JSON data from the internet using a GET request from the url
+     * @param url the url to fetch movies from the themoviedb.org API
+     * @return JSON data from API
+     * @throws IOException
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+
+            scanner.useDelimiter("\\A");
+            if (scanner.hasNext()) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+
+        } finally {
+                urlConnection.disconnect();
+        }
+    }
+
+}
