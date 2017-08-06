76baa0e63968862be25d6a9081603dc1a58866f8
aaroncrutchfield <aaroncrutchfield09@gmail.com>
2017-08-06 00:42:31 -0400

    Add proper checks to Intents

--- app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
@@ -1,8 +1,8 @@
 package com.example.android.popularmovies;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -27,27 +27,30 @@ public class MovieDetailsActivity extends AppCompatActivity {
         TextView overview = (TextView) findViewById(R.id.tv_overview);
         TextView releaseDate = (TextView) findViewById(R.id.tv_release_date);
 
-        Intent intent = getIntent();
-        int position = intent.getIntExtra("position", 0);
-        String moviePoster = intent.getStringExtra("moviePoster");
+        Intent intentFromMainActivity = getIntent();
 
-        ArrayList<JSONObject> movieObjectsArray = MainActivity.getMovieObjectsArray();
-        JSONObject movieObject = movieObjectsArray.get(position);
+        if (intentFromMainActivity.hasExtra("position") &&
+                intentFromMainActivity.hasExtra("moviePoster")) {
 
+            int position = intentFromMainActivity.getIntExtra("position", 0);
+            String moviePoster = intentFromMainActivity.getStringExtra("moviePoster");
 
-        Picasso.with(this)
-                .load(moviePoster)
-                .fit()
-                .into(detailsPoster);
+            ArrayList<JSONObject> movieObjectsArray = MainActivity.getMovieObjectsArray();
+            JSONObject movieObject = movieObjectsArray.get(position);
 
-        title.setText(JSONDataHandler.getDetails(movieObject, "title"));
-        try {
-            rating.setText(String.valueOf(movieObject.getDouble("vote_average")));
-        } catch (JSONException e) {
-            e.printStackTrace();
+            Picasso.with(this)
+                    .load(moviePoster)
+                    .fit()
+                    .into(detailsPoster);
+
+            title.setText(JSONDataHandler.getDetails(movieObject, "title"));
+            try {
+                rating.setText(String.valueOf(movieObject.getDouble("vote_average")));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            overview.setText(JSONDataHandler.getDetails(movieObject, "overview"));
+            releaseDate.setText(JSONDataHandler.getDetails(movieObject, "release_date"));
         }
-        overview.setText(JSONDataHandler.getDetails(movieObject, "overview"));
-        releaseDate.setText(JSONDataHandler.getDetails(movieObject, "release_date"));
-
     }
 }
