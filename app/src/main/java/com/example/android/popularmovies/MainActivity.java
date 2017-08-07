2cfcb56c1e65d0f20346dbee48f86e1b11ad35af
aaroncrutchfield <aaroncrutchfield09@gmail.com>
2017-08-06 23:46:19 -0400

    Check for internet connection and correct use of ApiInputError()

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.android.popularmovies;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -55,10 +60,22 @@ public class MainActivity extends AppCompatActivity {
         moviePosterAdapter = new MoviePosterAdapter();
         rvMoviePosters.setAdapter(moviePosterAdapter);
         //uncomment when using API set
-        apiKey = "35a2c8b5ef8960c539ecc989877bc80e";
-        getMovies();
-//        requestApiKey();
+//        apiKey = "35a2c8b5ef8960c539ecc989877bc80e";
+        requestApiKey();
         // TODO: 8/3/2017 check for internet connection before continuing
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        getMovies();
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
 
     }
 
@@ -80,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Prompts the user for their API Key. The dialog won't dismiss unless they enter one
      */
-    // TODO: 8/2/2017 returning null
     private void requestApiKey() {
 
         LayoutInflater layoutInflater = LayoutInflater.from(this);
@@ -104,13 +120,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //Notify the user we cannot continue without the API Key
-                        notifyApiInputError();        // TODO: 8/2/2017 handle API key error in onPostExecute
-                    }
-                })
+
                 .show();
     }
 
@@ -141,22 +151,32 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(movieRequestResults);
             //if nothing is returned, hide loadingIndicator. RecyclerView will remain empty
             showMoviePosters();
-            if (movieRequestResults == null){
-                notifyApiInputError();
-            } else {
+
                 // TODO: 8/2/2017 populate ArrayList to pass to RecyclerView.Adapter
                 try {
                     movieObjectsArray = JSONDataHandler.getMovieJSONObjectsArray(movieRequestResults);
                     ArrayList<Uri> posterLocationsArray = JSONDataHandler.getPosterLocationsArray(movieObjectsArray);
 
                     moviePosterAdapter.setMovieData(movieRequestResults, posterLocationsArray);
-                } catch (JSONException e) {
+                } catch (NullPointerException e) {     //catching JSONException and NullPointerException
+                    e.printStackTrace();
+                    if (isOnline()) {
+                        notifyApiInputError();
+                    }else {
+                        notifyConnectionError();
+                    }
+                } catch (JSONException e){
                     e.printStackTrace();
                 }
 
 
-            }
+
         }
+    }
+
+    private void notifyConnectionError() {
+        Toast.makeText(getApplicationContext(), "Please connect to the Internet to continue", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
     }
 
     @Override
