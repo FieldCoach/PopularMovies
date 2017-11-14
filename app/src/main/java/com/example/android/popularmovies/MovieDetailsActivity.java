90b485619f2b2bd8e690b9ae9885ea1f31e5edf4
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2017-11-14 03:59:49 -0500

    docs: Updated MovieDetailsActivity docs and comments

--- app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
@@ -49,23 +49,24 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
     private static final String TAG = MovieDetailsActivity.class.getSimpleName();
 
     private static final String MOVIE = "movie";
-    private static final int DETAILS_LOADER = 2;
     private static final String DETAILS_REQUEST_URL = "details_request_url";
+    private static final int DETAILS_LOADER = 2;
 
     private ActivityMovieDetailsBinding detailsBinding;
-    private ReviewAdapter reviewAdapter;
 
     private MovieTrailerAdapter movieTrailerAdapter;
+    private ReviewAdapter reviewAdapter;
 
-    private Boolean favorite = false;
     private ShareActionProvider mShareActionProvider;
     private Intent shareIntent;
     private Uri shareUri;
 
+    private Boolean favorite = false;
+
     /**
      * Gets the Intent from the MainActivity to retrieve Extras containing the details to display
      * in the UI
-     * @param savedInstanceState
+     * @param savedInstanceState contains data that was persisted
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,89 +80,47 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
 
             final Movie movie = intentFromMainActivity.getParcelableExtra(MOVIE);
 
-            //Find out if this movie is already a favorite
-            Cursor cursor = getContentResolver().query(MovieEntry.CONTENT_URI,
-                    null,
-                    MovieEntry.COLUMN_TITLE + "=?",
-                    new String[]{movie.getTitle()},
-                    null);
+            //Get the poster and backdrop
+            getPosterAndBackdrop(movie);
 
-            if (cursor.getCount() > 0){
-                Icon icon = Icon.createWithResource(this, R.drawable.ic_favorite_red_48dp);
-                detailsBinding.floatingActionButton.setImageIcon(icon);
-                favorite = true;
-            }
+            //Get the text details
+            getDetailsText(movie);
 
-            if (!favorite) {
-                //Load the poster into the ImageView
-                Picasso.with(this)
-                        .load(movie.getPosterLocationUriString())
-                        .fit()
-                        .into(detailsBinding.ivDetailsPoster);
+            //Load the trailers and reviews using the movieId
+            loadTrailersAndReviews(movie.getMovieId());
 
-                //Load the backdrop into its ImageView
-                Picasso.with(this)
-                        .load(movie.getBackdropLocationUriString())
-                        .fit()
-                        .into(detailsBinding.ivBackDrop);
-            } else {
-                // DONE: 11/10/2017 onCreate() - Load byteArrays from Movie object
-                cursor.moveToFirst();
-                Bitmap poster = bitmapFromBytes(cursor.getBlob(cursor.getColumnIndex(MovieEntry.COLUMN_POSTER)));
-                Bitmap backdrop = bitmapFromBytes(cursor.getBlob(cursor.getColumnIndex(MovieEntry.COLUMN_BACKDROP)));
-
-                detailsBinding.ivDetailsPoster.setImageBitmap(poster);
-                detailsBinding.ivBackDrop.setImageBitmap(backdrop);
-            }
-            cursor.close();
-
-
-            //Set the text on the TextView to show the Movie details
-            detailsBinding.inTitle.tvTitle.setText(movie.getTitle());
-            detailsBinding.inTitle.tvRating.setText(String.valueOf(movie.getVoteAverage()));
-            detailsBinding.inOverview.tvOverview.setText(movie.getOverview());
-
-            //Format the date before calling setText on the Text View
-            Date date = null;
-            try {
-                date = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(movie.getReleaseDate());
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            String dateString = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).format(date);
-
-            detailsBinding.inTitle.tvReleaseDate.setText(dateString);
-
-            //Construct URL for reviews using Movie movieId
-            getReviews(movie.getMovieId());
-
+            //Setup the reviews RecyclerView
             LinearLayoutManager layoutManager = new LinearLayoutManager(this);
             reviewAdapter = new ReviewAdapter(this);
 
             detailsBinding.inReviews.rvReviews.setLayoutManager(layoutManager);
             detailsBinding.inReviews.rvReviews.setAdapter(reviewAdapter);
 
-            LinearLayoutManager horizontalManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+            //Setup the movie trailer RecyclerView
+            LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
             movieTrailerAdapter = new MovieTrailerAdapter(this);
             SnapHelper snapHelper = new LinearSnapHelper();
 
-            detailsBinding.inTrailers.rvMovieTrailers.setLayoutManager(horizontalManager);
+            detailsBinding.inTrailers.rvMovieTrailers.setLayoutManager(horizontalLayoutManager);
             detailsBinding.inTrailers.rvMovieTrailers.setAdapter(movieTrailerAdapter);
             snapHelper.attachToRecyclerView(detailsBinding.inTrailers.rvMovieTrailers);
 
+            //Setup the add to favorites button
             detailsBinding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Icon icon;
+                    //Prevent favorites database changes offline
                     if (!NetworkUtils.isOnline(MovieDetailsActivity.this)) {
                         Toast.makeText(MovieDetailsActivity.this, "Favorites can't be modified offline", Toast.LENGTH_SHORT).show();
                         return;
                     }
+                    //Remove from favorites
                     if (favorite){
                         icon = Icon.createWithResource(view.getContext(), R.drawable.ic_favorite_black_48dp);
                         deleteFavoriteFromDb(movie.getTitle());
                         favorite = false;
+                    //Add to favorites
                     } else {
                         icon = Icon.createWithResource(view.getContext(), R.drawable.ic_favorite_red_48dp);
                         insertFavoriteToDb(movie);
@@ -174,21 +133,95 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
         }
     }
 
+    /**
+     * Populates the TextViews with details about the movie
+     * @param movie the movie object containing the details
+     */
+    private void getDetailsText(Movie movie) {
+        //Set the text on the TextViews to show the Movie details
+        detailsBinding.inTitle.tvTitle.setText(movie.getTitle());
+        detailsBinding.inTitle.tvRating.setText(String.valueOf(movie.getVoteAverage()));
+        detailsBinding.inOverview.tvOverview.setText(movie.getOverview());
+
+        //Format the date before calling setText on the Text View
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).parse(movie.getReleaseDate());
+            String dateString = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).format(date);
+            detailsBinding.inTitle.tvReleaseDate.setText(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads the poster and backdrop images from either the internet or the database depending on if
+     * the movie is a favorite
+     * @param movie the movie object whose images will be loaded
+     */
+    private void getPosterAndBackdrop(Movie movie) {
+        Cursor cursor = getContentResolver().query(MovieEntry.CONTENT_URI,
+                null,
+                MovieEntry.COLUMN_TITLE + "=?",
+                new String[]{movie.getTitle()},
+                null);
+
+        //If this movie is in the database, it is a favorite
+        if (cursor.getCount() > 0){
+            Icon icon = Icon.createWithResource(this, R.drawable.ic_favorite_red_48dp);
+            detailsBinding.floatingActionButton.setImageIcon(icon);
+            favorite = true;
+        }
+
+        //If this movie isn't a favorite, load the poster and backdrop from the internet
+        if (!favorite) {
+            //Load the poster into the ImageView
+            Picasso.with(this)
+                    .load(movie.getPosterLocationUriString())
+                    .fit()
+                    .into(detailsBinding.ivDetailsPoster);
+
+            //Load the backdrop into its ImageView
+            Picasso.with(this)
+                    .load(movie.getBackdropLocationUriString())
+                    .fit()
+                    .into(detailsBinding.ivBackDrop);
+        } else {
+            //If this movie is a favorite, load the images from the byteArrays in the database
+            cursor.moveToFirst();
+            Bitmap poster = bitmapFromBytes(cursor.getBlob(cursor.getColumnIndex(MovieEntry.COLUMN_POSTER)));
+            Bitmap backdrop = bitmapFromBytes(cursor.getBlob(cursor.getColumnIndex(MovieEntry.COLUMN_BACKDROP)));
+
+            detailsBinding.ivDetailsPoster.setImageBitmap(poster);
+            detailsBinding.ivBackDrop.setImageBitmap(backdrop);
+        }
+        cursor.close();
+    }
+
+    /**
+     * Removes a movie from the database by title
+     * @param title the movie title
+     */
     private void deleteFavoriteFromDb(String title) {
         String where = MovieEntry.COLUMN_TITLE + "=?";
         getContentResolver().delete(MovieEntry.CONTENT_URI, where, new String[]{title});
     }
 
+    /**
+     * Adds a movie to the database
+     * @param movie the movie to be added to the database
+     */
     private void insertFavoriteToDb(Movie movie) {
         ContentValues values = new ContentValues();
 
         try {
+            //Get the movie images from their URLs and store the data into byteArrays (to allow parceling)
             URL posterUrl = new URL(movie.getPosterLocationUriString());
             URL backdropUrl = new URL(movie.getBackdropLocationUriString());
 
             byte[] posterBytes = bytesFromImageUrl(posterUrl);
             byte[] backdropBytes = bytesFromImageUrl(backdropUrl);
 
+            //Add the movie details to the database
             values.put(MovieEntry.COLUMN_POSTER, posterBytes);
             values.put(MovieEntry.COLUMN_BACKDROP, backdropBytes);
             values.put(MovieEntry.COLUMN_PLOT, movie.getOverview());
@@ -205,6 +238,13 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
 
     //convert bitmap to byte array
     //https://stackoverflow.com/questions/11790104/how-to-storebitmap-image-and-retrieve-image-from-sqlite-database-in-android
+
+    /**
+     * Retrieves an byte array from the image's URL
+     * @param imageUrl the image URL location
+     * @return byte array
+     * @throws IOException if the URL is invalid
+     */
     private byte[] bytesFromImageUrl(URL imageUrl) throws IOException{
         InputStream inputStream = imageUrl.openStream();
         Bitmap image = BitmapFactory.decodeStream(inputStream);
@@ -213,11 +253,17 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
         return stream.toByteArray();
     }
 
+    /**
+     * Creates a bitmap from a byte array
+     * @param bytes byte array containing image data
+     * @return bitmap of the image
+     */
     private Bitmap bitmapFromBytes(byte[] bytes){
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
     }
 
-    private void getReviews(String movieId) {
+    private void loadTrailersAndReviews(String movieId) {
+        //Remove the trailers and reviews from the layout if there is no network connection
         if (!NetworkUtils.isOnline(this)){
             Toast.makeText(this,"Trailers and reviews offline can't be viewed offline", Toast.LENGTH_SHORT).show();
             detailsBinding.cvTrailers.setVisibility(View.GONE);
@@ -225,20 +271,24 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
             return;
         }
 
-        URL reviewsRequestUrl = NetworkUtils.buildReviewsDbUrl(movieId);
+        //Build the url to request the trailers and reviews
+        URL detailsRequestUrl = NetworkUtils.buildDetailsRequestUrl(movieId);
 
+        //Store the url in a bundle
         Bundle urlBundle = new Bundle();
-        urlBundle.putString(DETAILS_REQUEST_URL, reviewsRequestUrl.toString());
+        urlBundle.putString(DETAILS_REQUEST_URL, detailsRequestUrl.toString());
 
+        //Get the DETAILS_LOADER
         LoaderManager loaderManager = getSupportLoaderManager();
         Loader<String> detailsLoader = loaderManager.getLoader(DETAILS_LOADER);
 
+        //Initialize/Restart the DETAILS_LOADER and pass it the bundle containing the url
         if (detailsLoader == null) {
             loaderManager.initLoader(DETAILS_LOADER, urlBundle, this).forceLoad();
         } else {
             loaderManager.restartLoader(DETAILS_LOADER, urlBundle, this).forceLoad();
         }
-        Log.d(TAG, "getReviews: was called");
+        Log.d(TAG, "loadTrailersAndReviews: was called");
     }
 
     @Override
@@ -253,10 +303,13 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
             @Nullable
             @Override
             public String loadInBackground() {
+                //Get the details request url
                 String detailsRequestString = bundle.getString(DETAILS_REQUEST_URL);
 
+                //If the url is null, there is nothing to load
                 if (detailsRequestString == null || detailsRequestString.equals("")) return null;
 
+                //Load the JSON response from the url
                 try {
                     URL detailsRequestUrl = new URL(detailsRequestString);
                     String jsonResultString = NetworkUtils.getResponseFromHttpUrl(detailsRequestUrl);
@@ -273,12 +326,15 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
     @Override
     public void onLoadFinished(Loader<String> loader, String jsonResultString) {
         try {
+            //Save the reviews' data to an arrayList to send to its RecyclerView.Adapter
             ArrayList<Review> reviewArrayList = JSONDataHandler.getReviewArrayList(jsonResultString);
             reviewAdapter.setmReviewArrayList(reviewArrayList);
 
+            //Save the trailers' data to an arrayList to send to its RecyclerView.Adapter
             ArrayList<String> trailerArrayList = JSONDataHandler.getTrailerArrayList(jsonResultString);
             movieTrailerAdapter.setTrailerArrayList(trailerArrayList);
 
+            //Get the first trailer's YouTube url
             String BASE_URL = "https://www.youtube.com/watch";
             String trailerId = trailerArrayList.get(0);
 
@@ -286,25 +342,30 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
                     .appendQueryParameter("v", trailerId)
                     .build();
 
+            //Add the url to an intent
             shareIntent = new Intent(Intent.ACTION_SEND);
             shareIntent.setType("text/plain");
             shareIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
             shareIntent.putExtra(Intent.EXTRA_TEXT, shareUri.toString());
 
-        } catch (JSONException e) {
+        } catch (JSONException | NullPointerException e) {
             e.printStackTrace();
-        } catch (NullPointerException e){
-            //No reviews
         }
     }
 
     @Override
     public void onLoaderReset(Loader<String> loader) {
-
+        //Not implemented
     }
 
     //Add a share action
     //https://developer.android.com/reference/android/support/v7/widget/ShareActionProvider.html
+
+    /**
+     * Adds a share action to the actionBar
+     * @param menu the menu to be displayed
+     * @return true to create the menu
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_details, menu);
@@ -315,6 +376,10 @@ public class MovieDetailsActivity extends AppCompatActivity  implements LoaderMa
         return true;
     }
 
+    /**
+     * Sets the intent containing the data to be shared
+     * @param shareIntent the intent containing the data
+     */
     private void setShareIntent(Intent shareIntent){
         if (mShareActionProvider != null){
             mShareActionProvider.setShareIntent(shareIntent);
