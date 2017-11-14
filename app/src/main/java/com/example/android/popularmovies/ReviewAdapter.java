0769ea83c3a10331b8a34181d53cfcf8ec313f94
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2017-11-14 04:23:11 -0500

    docs: Updated ReviewAdapter docs and comments

--- app/src/main/java/com/example/android/popularmovies/ReviewAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/ReviewAdapter.java
@@ -32,13 +32,18 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewAdap
         ReviewAdapterViewHolder(View itemView) {
             super(itemView);
 
-            author = (TextView) itemView.findViewById(R.id.tv_author);
-            content = (TextView) itemView.findViewById(R.id.tv_content);
+            author = itemView.findViewById(R.id.tv_author);
+            content = itemView.findViewById(R.id.tv_content);
             view = itemView.findViewById(R.id.v_seperator);
         }
     }
 
-
+    /**
+     * Inflates the review_list_item layout then returns a ViewHolder containing that layout
+     * @param parent the parent ViewGroup
+     * @param viewType identifies the type of view
+     * @return ViewHolder containing the inflated layout
+     */
     @Override
     public ReviewAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
@@ -47,6 +52,11 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewAdap
         return new ReviewAdapterViewHolder(view);
     }
 
+    /**
+     * Sets the text on the TextViews in the ViewHolder
+     * @param holder the ViewHolder
+     * @param position the position of the ViewHolder to be bound
+     */
     @Override
     public void onBindViewHolder(ReviewAdapterViewHolder holder, int position) {
         Review review = mReviewArrayList.get(position);
@@ -59,13 +69,21 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewAdap
         }
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     * @return The total number of items in this adapter.
+     */
     @Override
     public int getItemCount() {
         if (null == mReviewArrayList) return 0;
         return mReviewArrayList.size();
     }
 
-    public void setmReviewArrayList(ArrayList<Review> mReviewArrayList) {
+    /**
+     * Clears previous data from the ArrayList of Reviews, adds new Reviews, then notifies the Adapter
+     * @param mReviewArrayList arrayList containing the reviews
+     */
+    void setmReviewArrayList(ArrayList<Review> mReviewArrayList) {
         this.mReviewArrayList.clear();
         this.mReviewArrayList.addAll(mReviewArrayList);
 
