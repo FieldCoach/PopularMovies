31921eac1f99bd99fe2ccccbf0a2e7523db527ce
androipirate <emrekalaycidesign@gmail.com>
2019-03-15 13:21:45 -0400

    Add Database annotation to MovieDatabase class.

--- app/src/main/java/com/example/android/popularmovies/data/MovieDatabase.java
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDatabase.java
@@ -2,9 +2,11 @@ package com.example.android.popularmovies.data;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Movie.class}, version=1)
 public abstract class MovieDatabase extends RoomDatabase {
     private static MovieDatabase INSTANCE;
     public abstract MovieDao movieDao();
