3e9b72a27a50fe2a26c8964128f5d2eee7216923
androipirate <emrekalaycidesign@gmail.com>
2019-03-15 12:26:16 -0400

    Add MovieDao interface for accessing database.

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDao.java
@@ -0,0 +1,30 @@
+package com.example.android.popularmovies.data;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+public interface MovieDao {
+
+    @Query("SELECT * FROM Movie")
+    LiveData<List<Movie>> getMovies();
+
+    @Query("SELECT * FROM Movie WHERE movieId = :id")
+    LiveData<Movie> getMovie(int id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertMovie(Movie movie);
+
+    @Insert(onConflict =  OnConflictStrategy.REPLACE)
+    void insertAllMovies(List<Movie> movies);
+
+    @Delete
+    void deleteMovie(Movie movie);
+
+    @Delete
+    void deleteAllMovies(List<Movie> movies);
+}
