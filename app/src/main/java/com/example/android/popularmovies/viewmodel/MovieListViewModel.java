0bcc05ffa40830f26d8e794c9ff585ef4cdf10a0
androipirate <emrekalaycidesign@gmail.com>
2019-07-01 13:04:45 -0400

    new feat - Implement new method to load more movies

--- app/src/main/java/com/example/android/popularmovies/viewmodel/MovieListViewModel.java
+++ app/src/main/java/com/example/android/popularmovies/viewmodel/MovieListViewModel.java
@@ -9,13 +9,21 @@ import com.example.android.popularmovies.repo.MovieRepository;
 import java.util.List;
 
 public class MovieListViewModel extends ViewModel {
+    private static final int PAGE_NUMBER_INCREMENT = 1;
     private MovieRepository repo;
+    private int pageNumber = 1;
 
     public MovieListViewModel() {
         this.repo = new MovieRepository();
+        repo.setPageNumber(pageNumber);
     }
 
     public LiveData<List<Movie>> getMovies() {
         return repo.getMoviesFromServer();
+    }
+
+    public void loadMovies() {
+        pageNumber += PAGE_NUMBER_INCREMENT;
+        repo.loadMoviesFromServer(pageNumber);
     }
 }
