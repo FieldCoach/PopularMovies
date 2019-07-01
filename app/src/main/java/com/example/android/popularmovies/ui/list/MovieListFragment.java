132e57a587b671298995c5ff83617d5152465ebe
androipirate <emrekalaycidesign@gmail.com>
2019-07-01 13:06:55 -0400

    refactor - Remove unnecessary import statements in MovieListFragment

--- app/src/main/java/com/example/android/popularmovies/ui/list/MovieListFragment.java
+++ app/src/main/java/com/example/android/popularmovies/ui/list/MovieListFragment.java
@@ -23,32 +23,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.android.popularmovies.ApiKeyFile;
 import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.data.Movie;
-import com.example.android.popularmovies.data.Movies;
 import com.example.android.popularmovies.ui.PosterAdapter;
 import com.example.android.popularmovies.ui.details.MovieDetailsFragment;
 import com.example.android.popularmovies.utilities.EndlessRecyclerViewScrollListener;
-import com.example.android.popularmovies.utilities.MovieDbService;
 import com.example.android.popularmovies.utilities.NetworkUtils;
 import com.example.android.popularmovies.viewmodel.MovieListViewModel;
 
